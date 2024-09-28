befa518726e4423230b37a9f5db1970cd7fcaad4
Rahul <rahultime2018@gmail.com>
2024-09-28 15:19:13 +0530

    .

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ closeMenuButton.addEventListener("click", function(){
     sidebar.style.display = "none"
     menuButton.style.display = "block"
 })
+
+let lastScrollTop = 0;
+const header = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function() {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+        header.style.transform = 'translateY(0)'; // Hide header on scroll down
+    } else {
+        header.style.transform = 'translateY(0)'; // Show header on scroll up
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+});
